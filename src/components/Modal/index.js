b86263761dd57d261d3ab2193064e75a81a01e7e
Eduardo <agenciacriareof@gmail.com>
2021-12-06 15:08:29 -0300

    resolvendo erros de rota da infinityfree

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,3 +1,91 @@
+import React, { useState } from "react";
+import { ModalArea } from "./styled";
+import Search from "@material-ui/icons/Search";
+import { useHistory } from "react-router-dom";
+export default function Modal({ close, open }) {
+  const [word, setWord] = useState("");
+  let history = useHistory();
+  function handleClose(e) {
+    if (e.target.classList.contains("fundo")) {
+      close(false);
+    }
+  }
+  function handleForm(e) {
+    e.preventDefault();
+    close(false);
+    return history.push({
+      pathname: "/search",
+      search: `?query=${word}`,
+    });
+  }
+  return (
+    <ModalArea className="fundo" search={open} onClick={handleClose}>
+      <h2>Busque pelo nome do filme</h2>
+      <div className="search">
+        <form method="GET" onSubmit={handleForm}>
+          <input
+            name="query"
+            type="text"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+            required
+            placeholder="Ex: Velozes e furiosos"
+          />
+          <button type="submit">
+            <Search />
+          </button>
+        </form>
+      </div>
+    </ModalArea>
+  );
+}
+
+/*
+import React, { useState } from "react";
+import { ModalArea } from "./styled";
+import Search from "@material-ui/icons/Search";
+import { useHistory } from "react-router-dom";
+export default function Modal({ close, open }) {
+  const [word, setWord] = useState("");
+  let history = useHistory();
+  function handleClose(e) {
+    if (e.target.classList.contains("fundo")) {
+      close(false);
+    }
+  }
+  function handleForm(e) {
+    e.preventDefault();
+    close(false);
+    return history.push({
+      pathname: "/search",
+      search: `?query=${word}`,
+    });
+  }
+  return (
+    <ModalArea className="fundo" search={open} onClick={handleClose}>
+      <h2>Busque pelo nome do filme</h2>
+      <div className="search">
+        <form method="GET" onSubmit={handleForm}>
+          <input
+            name="query"
+            type="text"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+            required
+            placeholder="Ex: Velozes e furiosos"
+          />
+          <button type="submit">
+            <Search />
+          </button>
+        </form>
+      </div>
+    </ModalArea>
+  );
+}
+
+
+velho
+
 import React from "react";
 import { ModalArea } from "./styled";
 import Search from "@material-ui/icons/Search";
@@ -27,3 +115,5 @@ export default function Modal({ close, open }) {
     </ModalArea>
   );
 }
+
+*/
--- src/components/SliderMovie/index.js
+++ src/components/SliderMovie/index.js
@@ -1,26 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { SliderMovieArea } from "./styled";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 export default function SliderMovie({ data, noSlider, serie, similar }) {
+  let history = useHistory();
   const handleDragStart = (e) => e.preventDefault();
+
+  function handleChangeMovie(id) {
+    if (serie) {
+      history.push("");
+      setTimeout(() => history.push(`/serie/${id}`), 0);
+    } else {
+      history.push("");
+      setTimeout(() => history.push(`/filme/${id}`), 0);
+    }
+  }
   const items =
     data.items.results.length > 0 &&
     data.items.results.map((item, k) => (
       <>
         {similar && (
           <div key={k}>
-            <a href={serie ? `/serie/${item.id}` : `/filme/${item.id}`}>
+            <div onClick={() => handleChangeMovie(item.id)}>
               <img
                 onDragStart={handleDragStart}
                 src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`}
                 alt={item.original_name}
               />
-            </a>
-            <a href={serie ? `/serie/${item.id}` : `/filme/${item.id}`}>
+            </div>
+            <div onClick={() => handleChangeMovie(item.id)}>
               <div className="title">{serie ? item.name : item.title}</div>
-            </a>
+            </div>
             <div className="infoSlider">
               <span>
                 Data: {serie ? item.first_air_date : item.release_date}
@@ -124,4 +135,30 @@ export default function SliderMovie({ data, noSlider, serie, similar }) {
   );
 }
 
-//https://www.npmjs.com/package/react-alice-carousel
+/*
+{similar && (
+          <div key={k}>
+            <a href={serie ? `/serie/${item.id}` : `/filme/${item.id}`}>
+              <img
+                onDragStart={handleDragStart}
+                src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`}
+                alt={item.original_name}
+              />
+            </a>
+            <a href={serie ? `/serie/${item.id}` : `/filme/${item.id}`}>
+              <div className="title">{serie ? item.name : item.title}</div>
+            </a>
+            <div className="infoSlider">
+              <span>
+                Data: {serie ? item.first_air_date : item.release_date}
+              </span>
+              <span>
+                <span style={{ color: "#ff0066", fontWeight: "bold" }}>
+                  IMDb:
+                </span>
+                {item.vote_average}
+              </span>
+            </div>
+          </div>
+        )}
+*/
